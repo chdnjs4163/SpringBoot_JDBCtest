cf6074e55b2a66eafffe55a2106dfaac5417d085
chdnjs4163 <gufvnd45@naver.com>
2025-08-25 11:48:09 +0900

    JDBC 연습1

---
+++ src/main/java/kr/ac/kopo/won/springboot_jdbctest/SpringBootJdbCtestApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.won.springboot_jdbctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbCtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJdbCtestApplication.class, args);
+    }
+
+}

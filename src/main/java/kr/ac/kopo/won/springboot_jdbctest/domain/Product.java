1501595b8590df79a98854e061527360b845c53a
chdnjs4163 <gufvnd45@naver.com>
2025-09-22 11:49:06 +0900

    @OneToOne 02 공유키 양방향, @Maby TO One

---
+++ src/main/java/kr/ac/kopo/won/springboot_jdbctest/domain/Product.java
@@ -0,0 +1,23 @@
+package kr.ac.kopo.won.springboot_jdbctest.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private float price;
+//    단방향: Product에서 Detail를 참조
+//    @OneToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "detail_id")
+//    private Detail detail;
+
+//    공유기본키를 사용한 양방향
+    @OneToOne( mappedBy = "product", cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
+    private Detail detail;
+}
